6e85311b0bac5dfe3e403ef29feb02fac3b635fd
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-04-26 13:34:56 -0400

    :sparkles: try to render a MD version

--- build.py
+++ build.py
@@ -1,9 +1,24 @@
 import subprocess
 import json
 
-subprocess.run(["ls", "-l"])
-
 with open(".zenodo.json", 'r') as f:
     zenodo = json.load(f)
 
 print(zenodo)
+
+root = [
+    "pandoc",
+    "manuscript.md"
+]
+
+common_flags = [
+    "--filter", "pandoc-fignos",
+    "--filter", "pandoc-eqnos",
+    "--filter", "pandoc-tablenos",
+    "--filter", "pandoc-citeproc",
+    "--bibliography", "references.json"
+]
+
+# Version for the website
+subprocess.run(root + ["-o", "index.md", "--template", "templates/index.md"] + common_flags)
+subprocess.run(["cat", "index.md"])
---
+++ templates/index.md
@@ -0,0 +1,7 @@
+# $title$
+
+$for(creators)$
+$creators.familyname$, $creators.givennames$ -- $creators.affiliation$
+$endfor$
+
+$body$
