fb3139d6669970a968b07c8c18a434f4824f643e
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-04-26 14:14:29 -0400

    :bug: no need for pandoc-citeproc

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb
 sudo dpkg -i pandoc-2.7.2-1-amd64.deb
 
-sudo apt-get install pandoc-citeproc -y
+# sudo apt-get install pandoc-citeproc -y
 
 pip install -r barlock/requirements.txt
