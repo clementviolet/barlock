da3f3c5ed328b114795dd0accef29f99676107e6
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-07-12 09:52:07 -0400

    update build

--- build.py
+++ build.py
@@ -8,30 +8,30 @@ with open(".zenodo.json", 'r') as f:
 filename = "_".join(zenodo["title"].split()[:6])
 
 root = [
-    "pandoc",
-    "manuscript.md"
-]
+        "pandoc",
+        "manuscript.md"
+        ]
 
-    subprocess.run("mkdir" + "-p" + current_branch)
+subprocess.run("mkdir" + "-p" + current_branch)
 common_flags = [
-    "--filter", "pandoc-fignos",
-    "--filter", "pandoc-eqnos",
-    "--filter", "pandoc-tablenos",
-    "--filter", "pandoc-citeproc",
-    "--bibliography=references.json",
-    "--metadata-file=.zenodo.json",
-    "--variable=filename:" + filename
-]
+        "--filter", "pandoc-fignos",
+        "--filter", "pandoc-eqnos",
+        "--filter", "pandoc-tablenos",
+        "--filter", "pandoc-citeproc",
+        "--bibliography=references.json",
+        "--metadata-file=.zenodo.json",
+        "--variable=filename:" + filename
+        ]
 
 versions = {
-    "index.html": ["--template", "barlock/templates/index.html", "--webtex"],
-    filename + ".pdf": [],
-    filename + ".tex": [],
-    filename + ".docx": []
-}
+        "index.html": ["--template", "barlock/templates/index.html", "--webtex"],
+        filename + ".pdf": [],
+        filename + ".tex": [],
+        filename + ".docx": []
+        }
 
 current_branch = os.getenv('TRAVIS_BRANCH', 'master')
-if current_branch != "master":
+if (current_branch != "master"):
     subprocess.run("mkdir" + "-p" + current_branch)
 
 print("BUILD FOR " + current_branch)
