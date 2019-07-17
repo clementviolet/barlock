938710c713d837a056ec53f26e1d6205a514c7ed
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-07-17 10:23:16 -0400

    :bug: ams fonts

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 wget -qO- "https://yihui.name/gh/tinytex/tools/install-unx.sh" | sh
 tlmgr install xcolor
 tlmgr install mdwtools
+tlmgr install amsfonts
 
 # Pandoc 2.7.2
 wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb
