cfcb987df514769c4a85e478950b1986074a155c
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-07-18 15:32:31 -0400

    :arrow_up: pandoc 2.7.3

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,9 @@ tlmgr install mdwtools
 tlmgr install amsfonts
 
 # Pandoc 2.7.2
-wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb
-sudo dpkg -i pandoc-2.7.2-1-amd64.deb
-rm pandoc-2.7.2-1-amd64.deb
+wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-1-amd64.deb
+sudo dpkg -i pandoc-2.7.3-1-amd64.deb
+rm pandoc-2.7.3-1-amd64.deb
 
 # Python requirements (pandoc filters)
 pip install -r barlock/requirements.txt
