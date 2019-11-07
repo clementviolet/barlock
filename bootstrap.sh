f58868db2c127a79145643eedb9b91ca6bf66854
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-07 09:33:58 +0100

    Custom LaTeX engine and template for pdf output.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,22 @@
 #! /bin/bash
 
 # TinyTeX
-wget -qO- "https://yihui.name/gh/tinytex/tools/install-unx.sh" | sh
-tlmgr install xcolor
-tlmgr install mdwtools
-tlmgr install amsfonts
+#wget -qO- "https://yihui.name/gh/tinytex/tools/install-unx.sh" | sh
+# sudo apt-get install tex-common
+# sudo apt-get install texlive-base
+# sudo apt-get install texlive-binaries
+# sudo apt-get install texlive-luatex
+# sudo apt-get install texlive-luatex
+# sudo apt-get install texlive-full
+# tlmgr install xcolor
+# tlmgr install mdwtools
+# tlmgr install amsfonts
+# ###
+# tlmgr install unicode-math
+# tlmgr install lualatex-math
+# tlmgr install filehook
+# tlmgr install lm-math # Font
+
 
 # Pandoc 2.7.2
 wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-1-amd64.deb
